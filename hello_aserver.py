f668486672f5db10c3de020aa7fb5f8a7b695258
Mathieu Lecarme <mlecarme@bearstech.com>
2017-01-20 22:13:05 +0100

    Sleep to see async beahvior

--- hello_aserver.py
+++ hello_aserver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from asyncio import get_event_loop, start_unix_server
+from asyncio import get_event_loop, start_unix_server, sleep
 from test_pb2 import Hello, World
 
 from ascetic_rpc.async.server import Server
@@ -10,6 +10,7 @@ class Test:
 
     async def mob(self, hello: Hello) -> World:
         for name in u"Eugesipe Adélard Ladislas".split(" "):
+            await sleep(1)
             yield World(Message=name)
 
     async def hello(self, hello: Hello) -> World:
